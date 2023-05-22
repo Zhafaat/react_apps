ee0be32ced230196915bd0b39a31c374ec316fab
zhafaat <ilmuumum18@gmail.com>
2023-05-22 14:38:25 +0800

    porto ini sudah selesai di component Bussiness, selanjutnya masuk ke komponent Billing. tutorial di menit ke 1:27:01.

--- bank_modern_app/src/components/index.js
+++ bank_modern_app/src/components/index.js
@@ -6,7 +6,6 @@ import Clients from "./Clients";
 import CTA from "./CTA";
 import FeedbackCard from "./FeedbackCard";
 import Footer from "./Footer";
-import GetStarted from "./GetStarted";
 import Hero from "./Hero";
 import Navbar from "./Navbar";
 import Stats from "./Stats";
@@ -21,7 +20,6 @@ export {
     CTA,
     FeedbackCard,
     Footer,
-    GetStarted,
     Hero,
     Navbar,
     Stats,
